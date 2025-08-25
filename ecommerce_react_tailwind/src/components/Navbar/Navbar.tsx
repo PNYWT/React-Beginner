c7898cc80ce7668d5b1998a0e9b8e8fb94a7a4ac
CallmeOni <99156196+PNYWT@users.noreply.github.com>
2025-08-25 12:13:13 +0700

    done

--- ecommerce_react_tailwind/src/components/Navbar/Navbar.tsx
+++ ecommerce_react_tailwind/src/components/Navbar/Navbar.tsx
@@ -51,36 +51,35 @@ const DropdownLinks = [
   },
 ];
 
-const Navbar = () => {
+const Navbar = ({ handleOrderPopup }: { handleOrderPopup: () => void }) => {
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
-      {/* upper Navbar */}
+      {/* Upper Navbar */}
       <div className="bg-primary/40 py-2">
-        <div className="w-full flex justify-between items-center px-16">
+        <div className="container flex justify-between items-center">
           <div>
-            <a
-              href="#"
-              className="font-bold text-2xl sm:text-3xl flex gap-2 pl-4"
-            >
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
               <img src={Logo} alt="Logo" className="w-10" />
-              Call me Ecommerce
+              Callme Ecommerce
             </a>
           </div>
 
-          {/* search bar */}
+          {/* Search bar */}
           <div className="flex justify-between items-center gap-4">
             <div className="relative group hidden sm:block">
               <input
                 type="text"
                 placeholder="search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800  "
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 
+                bg-white
+                dark:bg-gray-800  "
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
 
-            {/* order button */}
+            {/* Order button */}
             <button
-              onClick={() => alert("Order Button Clicked")}
+              onClick={() => handleOrderPopup()}
               className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
             >
               <span className="group-hover:block hidden transition-all duration-200">
@@ -96,7 +95,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      {/* lower Navbar */}
+      {/* Lower Navbar */}
       <div data-aos="zoom-in" className="flex justify-center">
         <ul className="sm:flex hidden items-center gap-4">
           {Menu.map((data) => (
